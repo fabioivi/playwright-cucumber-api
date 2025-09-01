f9ccbb126137713ffb2722fdcbbd94ef12adff00
Fábio Duarte <fabio.ivi@gmail.com>
2025-09-01 10:10:47 -0400

    add support to import in cucumber

--- cucumber.mjs
+++ cucumber.mjs
@@ -1,2 +1,2 @@
 
-export default `--require tests/steps/*.js tests/features/*.feature --format progress --format json:cucumber_report.json`;
+export default `--import tests/steps/*.js tests/features/*.feature --format progress --format json:cucumber_report.json`;
